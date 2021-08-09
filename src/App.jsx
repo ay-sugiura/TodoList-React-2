0dcb786c8f67dae4225398c55eec2b89b974e4bf
ay-sugiura <65064434+ay-sugiura@users.noreply.github.com>
2021-08-09 14:58:00 +0900

    べた書きモック

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./styles.css";
+
+export const App = () => {
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <li>
+            <div className="list-row">
+              AAAAAA
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+          <li>
+            <div className="list-row">
+              IIII
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          <li>
+            <div className="list-row">
+              ああああああ
+              <button>戻す</button>
+            </div>
+          </li>
+          <li>
+            <div className="list-row">
+              いいいいい
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
